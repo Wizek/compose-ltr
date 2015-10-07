1a072bb79c97a9cac65f00ed1d610dccf00a47f1
Milán Nagy <123.wizek@gmail.com>
2015-10-07 13:31:41 +0200

    Added more examples to specs

--- spec/Spec.hs
+++ spec/Spec.hs
@@ -10,14 +10,24 @@ main = hspec $ do
   describe "compose-ltr" $ do
 
     describe "$>" $ do
-      it "should work" $ do
+      it "should compose" $ do
+
+        (1 $> (+2) $> (*2)) `shouldBe` 6
+
+        "Hello World"
+          $> reverse
+          $> ("!" ++)
+          $> take 6
+          $> (`shouldBe` "!dlroW")
+
+      it "should work just like the Prelude version" $ do
         let
           prop :: (Fun Int Int) -> Int -> Bool
           prop (Fun _ f) g = prop1 f g
         property prop
 
     describe ".>" $ do
-      it "should work" $ do
+      it "should work just like the Prelude version" $ do
         let
           prop :: (Fun Int Int) -> (Fun Int Int) -> Bool
           prop (Fun _ f) (Fun _ g) = prop2 f g
